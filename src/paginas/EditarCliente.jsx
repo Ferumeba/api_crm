7946602f401af75d4f14ffdecdfbfe2307a7e449
ferumeba <ferumeba@hotmail.com>
2022-07-24 09:30:04 -0600

    first commit

---
+++ src/paginas/EditarCliente.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Formulario from '../components/Formulario';
+
+const EditarCliente = () => {
+  const { id } = useParams();
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+
+      setCargando(!cargando);
+      // para ver el spinner...
+      // setTimeout(() => {
+      //   setCargando(!cargando);
+      // }, 3000);
+    };
+
+    obtenerClienteAPI();
+  }, []);
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+      <p className="mt-3">
+        Utiliza este formulario para editar los datos de un cliente
+      </p>
+
+      {cliente?.nombre ? (
+        <Formulario cliente={cliente} cargando={cargando} />
+      ) : (
+        <p>Cliente ID no válido</p>
+      )}
+    </>
+  );
+};
+
+export default EditarCliente;
